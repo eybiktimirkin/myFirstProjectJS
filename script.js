96f3fc77579fb039acca65442e8c1011d978f6c2
Evgenii Biktimirkin <biktizhenya997@mail.ru>
2025-08-25 21:11:54 +0400

    это мой первый коммит в новой ветке

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello");
+const title = "мой первый проект";
+let screens = "экраны";
+let screenPrice = 1000;
+let rollback = "снижение цен";
+let fullPrice = 2000;
+let adaptive = "адаптив";
+
+alert("Привет!");
+console.log("Hello!");
