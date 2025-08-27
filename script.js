836164cce008028318d4dce08fbea25caa3e4e01
Evgenii Biktimirkin <biktizhenya997@mail.ru>
2025-08-27 21:51:26 +0400

    add: lesson 2

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const title = "мой первый проект";
-let screens = "экраны";
+let title = "Мой первый проект";
+let screens = "Простые, сложные, Интерактивные";
 let screenPrice = 1000;
-let rollback = "снижение цен";
+let rollback = 88;
 let fullPrice = 2000;
-let adaptive = "адаптив";
+let adaptive = true;
 
-alert("Привет!");
-console.log("Hello!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "рублей");
+console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей");
+console.log(screens.toLowerCase());
+console.log(screens.split(" "));
+console.log(
+  "Процент отката посреднику за работу" + " " + (fullPrice * rollback) / 100
+);
