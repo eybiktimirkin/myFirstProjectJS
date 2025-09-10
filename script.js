c811160342e9bf9dbeb04b23a756fe2f92b33aab
Evgenii Biktimirkin <biktizhenya997@mail.ru>
2025-09-10 21:08:08 +0400

    add lesson05

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 "use strict";
 
-let title = prompt("Как называется Ваш проект?");
-let screens = prompt("Какие типы экранов нужно разработать");
-let screenPrice = +prompt("Сколько будет стоить данная работа", "12000");
-let adaptive = confirm("Нужен ли адаптив на сайте");
-let service1 = prompt("Какой дополнительный тип услуги нужен");
-let servicePrice1 = +prompt("Сколько это будет стоить");
-let service2 = prompt("Какой дополнительный тип услуги нужен");
-let servicePrice2 = +prompt("Сколько это будет стоить");
+let title;
+let screens;
+let screenPrice;
+let adaptive;
 let rollback = 10;
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
+let fullPrice;
+let servicePercentPrice;
 let allServicePrices;
+let service1;
+let service2;
+
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+const asking = function () {
+  title = prompt("Как называется Ваш проект?", "Калькулятор верстки");
+  screens = prompt("Какие типы экранов нужно разработать", "Простые, сложные");
+  do {
+    screenPrice = +prompt("Сколько будет стоить данная работа");
+  } while (!isNumber(screenPrice));
+
+  adaptive = confirm("Нужен ли адаптив на сайте");
+};
 
 const showTypeOf = function (variable) {
   console.log(variable, typeof variable);
@@ -28,18 +40,33 @@ const getRollBackMessage = function (price) {
   }
 };
 const getAllServicePrices = function () {
-  return servicePrice1 + servicePrice2;
+  let sum = 0;
+  for (let i = 0; i < 2; i++) {
+    if (i === 0) {
+      service1 = prompt("Какой дополнительный тип услуги нужен");
+    } else if (i === 1) {
+      service2 = prompt("Какой дополнительный тип услуги нужен");
+    }
+
+    let price;
+    do {
+      price = prompt("Сколько это будет стоить");
+    } while (!isNumber(price));
+    sum += +price;
+  }
+  return sum;
 };
 const getFullPrice = function () {
   return screenPrice + allServicePrices;
 };
 const getTitle = function () {
-  return title.trim()[0].toUpperCase() + title.trim().subStr(1).toLowerCase();
+  return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase();
 };
 const getServicePercentPrice = function () {
   return fullPrice - fullPrice * (rollback / 100);
 };
 
+asking();
 allServicePrices = getAllServicePrices();
 fullPrice = getFullPrice();
 title = getTitle();
@@ -49,6 +76,7 @@ showTypeOf(title);
 showTypeOf(screenPrice);
 showTypeOf(adaptive);
 
+console.log("allServicePrices", allServicePrices);
 console.log(getRollBackMessage(fullPrice));
 console.log(typeof title);
 console.log(typeof screenPrice);
@@ -56,5 +84,5 @@ console.log(typeof adaptive);
 console.log(screens.length);
 console.log(servicePercentPrice);
 console.log(
-  "Стоимость верстки экранов" + screenPrice + "рублей/долларов/гривен/юани"
+  "Стоимость верстки экранов " + screenPrice + " рублей/ долларов/ гривен/ юани"
 );
